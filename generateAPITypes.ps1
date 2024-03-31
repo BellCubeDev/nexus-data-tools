ecd2b1a4ae7945fc3fa93e17cedef9107c4117db
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2024-03-30 22:52:23 -0400

    Move types-graphql-builder call into package.json
    The `src/nexus-api folder` is tracked so it should always be created.

--- generateAPITypes.ps1
+++
@@ -1,7 +0,0 @@
-New-Item -ItemType Directory -Force -Path './src/nexus-api/'
-try {
-    Write-Host "Generating GraphQL.ts"
-    npx typed-graphql-builder --schema https://api.nexusmods.com/v2/graphql --output './src/nexus-api/GraphQL.ts'
-} catch {
-    Write-Host "Failed to generate GraphQL.ts" $_
-}
